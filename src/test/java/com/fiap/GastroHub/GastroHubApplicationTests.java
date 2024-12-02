76a84ccaed85be637dbe48def943e39c3e21da5c
Gaddini <ferreiragaddini@bol.com.br>
2024-12-02 19:22:45 -0300

    Changed test class name

--- src/test/java/com/fiap/GastroHub/GastroHubApplicationTests.java
+++ src/test/java/com/fiap/GastroHub/GastroHubApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LocatechApplicationTests {
+class GastroHubApplicationTests {
 
 	@Test
 	void contextLoads() {
