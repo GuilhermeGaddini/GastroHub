b7c7c9e5f204e33f70c00b948010021fdd0b4fd0
Gaddini <ferreiragaddini@bol.com.br>
2025-03-19 21:17:18 -0300

    TESTES - Remoção de imports não utilizados e comentários

--- GastroHub/src/test/java/com/fiap/GastroHub/modules/products/infra/http/ProductControllerTest.java
+++ GastroHub/src/test/java/com/fiap/GastroHub/modules/products/infra/http/ProductControllerTest.java
@@ -11,25 +11,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc(addFilters = false)
-//@WebMvcTest(controllers = ProductController.class)
 public class ProductControllerTest {
-//    private MockMvc mockMvc;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -39,14 +30,9 @@ public class ProductControllerTest {
     @InjectMocks
     private ProductController productController;
 
-//    public ProductControllerTest(MockMvc mockMvc) {
-//        this.mockMvc = mockMvc;
-//    }
-
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
     }
 
     @Test
